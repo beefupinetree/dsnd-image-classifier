eef0cb0cf5c6d4ceaed50227c0a3d01b497c577c
Omar Samhan <omar.samhan@mail.utoronto.ca>
2018-11-24 23:00:12 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 24 22:56:54 2018
+
+@author: tareksamhan
+"""
+
+from setuptools import setup
+
+setup(name='nn-classifier',
+      version='0.1',
+      description='Generic image classifiers',
+      packages=['classifier'],
+      zip_safe=False)
